ff4975d52a6c5a997859afedf3ae99736db1db2b
s3662507 <s3662507@student.rmit.edu.au>
2021-05-21 00:32:05 +1000

    Elastic Beanstalk Semi Working, Timeouts from Frontend to Backend

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,2 +1,5 @@
-export const API_URL = 'http://localhost:8080'
-export const JPA_URL = 'http://localhost:8080/jpa'
+//export const API_URL = 'http://localhost:8080'
+//export const JPA_URL = 'http://localhost:8080/jpa'
+
+export const API_URL = 'http://neservice.ap-southeast-2.elasticbeanstalk.com:8080'
+export const JPA_URL = 'http://neservice.ap-southeast-2.elasticbeanstalk.com:8080/jpa'
