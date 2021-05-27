147805352810ce5d6ca044d3fd706236b6eb8534
s3662507 <s3662507@student.rmit.edu.au>
2021-05-28 01:47:41 +1000

    AWS Elastic Beanstalk Working

--- server/src/main/java/com/neservice/GlobalVariable.java
+++ server/src/main/java/com/neservice/GlobalVariable.java
@@ -3,5 +3,5 @@ package com.neservice;
 public class GlobalVariable {
 	// Global Variable for the Application URL
 	//public static final String APP_URL = "http://localhost";
-	public static final String APP_URL = "http://neservice.ap-southeast-2.elasticbeanstalk.com/";
+	public static final String APP_URL = "http://neservice.ap-southeast-2.elasticbeanstalk.com";
 }
