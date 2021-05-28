0ac859c76b32b3ad6c32ebf6cd7afa5492074ba3
Karen <karentkcheung@gmail.com>
2021-05-28 21:03:50 +0800

    friday eve.1

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <Link>History</Link>
+            <Link>My service</Link>
+            <Link></Link>
+            <Link></Link>
+        </div>
+    )
+}
+
+export default Navbar
