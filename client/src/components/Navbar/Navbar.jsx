b23601ccfff7545cf1da40f1bedee23f887f81d8
Karen <karentkcheung@gmail.com>
2021-06-05 18:30:12 +0800

    service pages update and nav bar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import AuthenticationService from '../Authentication/AuthenticationService.js';
+import {useHistory, useLocation} from "react-router-dom";
+
 
 const Nav = styled.nav`
     background: #28254F;
@@ -16,19 +18,37 @@ const NavLink = styled(Link)`
     color: #BDACE6;
     font-family: 'Raleway';
     text-decoration: none;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-size: 20px;
 `
 
-const logOut = () => {
-    AuthenticationService.logout()
-}
+const LogoutBtn = styled.button`
+    color: #BDACE6;
+    font-family: 'Raleway';
+    text-decoration: none;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    background: none;
+    border: none;
+    font-size: 20px;
 
+`
 
-function Navbar() {
+const Navbar = () => {
+
+    let history = useHistory();
+
+    const logOut = () => {
+        sessionStorage.removeItem('authenticatedUser');
+        history.push('/');
+    }
+
     return (
         <Nav>
             <NavLink to="">History</NavLink>
             <NavLink to="/service">My service</NavLink>
-            <NavLink to="/" {...logOut()}>Log out</NavLink>
+            <LogoutBtn onClick={() => logOut()}>Log out</LogoutBtn>
         </Nav>
     )
 }
