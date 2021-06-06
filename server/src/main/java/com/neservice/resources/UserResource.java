72d809c64c696961f77c8ff53c55d993543fc37c
s3662507 <s3662507@student.rmit.edu.au>
2021-06-07 01:55:14 +1000

    Update User Password Completed

--- server/src/main/java/com/neservice/resources/UserResource.java
+++ server/src/main/java/com/neservice/resources/UserResource.java
@@ -2,6 +2,8 @@ package com.neservice.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +21,7 @@ import com.neservice.repository.LoginRepo;
 public class UserResource {
 	@Autowired
 	private LoginRepo repo;	
-	
+
 	@GetMapping("/jpa/{email}")
 	public UserBasic getUser(@PathVariable String email){
 		return new UserBasic(repo.findByEmail(email));
@@ -31,7 +33,7 @@ public class UserResource {
 	}
 	
 	@PostMapping("/jpa/{email}")
-	public ResponseEntity<Void> updateUser (@PathVariable String email, @RequestBody UserBasic user){
+	public ResponseEntity<Void> updateUser(@PathVariable String email, @RequestBody UserBasic user){
 		// Used to Store Retrieved Data to Database
 		User db = repo.findByEmail(email);
 		
@@ -44,6 +46,32 @@ public class UserResource {
 		
 		repo.save(db);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping("/jpa/{email}/{oldPassword}/update")
+	public boolean updatePassword(
+			@PathVariable String email, 
+			@PathVariable String oldPassword,
+			@RequestBody User user
+		){
+		boolean passwordCorrect = false;
+		
+		// Used to Store Retrieved Data to Database
+		User db = repo.findByEmail(email);
+		
+		// Test and Save the Password
+		BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();  		
+		user.setPassword(bCrypt.encode(user.getPassword()));
+		
+		if(bCrypt.matches(oldPassword, db.getPassword())) {
+			passwordCorrect = true;
+			
+			db.setPassword(user.getPassword());
+			
+			repo.save(db);
+		}
+		
+		return passwordCorrect;
 	}
 
 }
