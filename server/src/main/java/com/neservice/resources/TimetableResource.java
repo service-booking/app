ef4be48cc0ed551e2c058e2eb1c46058d7d54cb7
s3662507 <s3662507@student.rmit.edu.au>
2021-06-07 02:22:27 +1000

    Minor Removal of Unused Code

--- server/src/main/java/com/neservice/resources/TimetableResource.java
+++ server/src/main/java/com/neservice/resources/TimetableResource.java
@@ -1,7 +1,6 @@
 package com.neservice.resources;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- server/src/main/java/com/neservice/resources/UserResource.java
+++ server/src/main/java/com/neservice/resources/UserResource.java
@@ -3,7 +3,6 @@ package com.neservice.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
